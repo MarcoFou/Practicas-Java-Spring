4ae5430040f91ba937d6c3afe0a6b6077274bdbc
Marco Fouad <abboud1995@gmail.com>
2023-01-03 07:13:13 -0300

    HelloWorld en Spring Framework

---
+++ spring-helloworld/src/main/java/com/example/CalculatorService.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class CalculatorService {
+
+
+    public CalculatorService() {
+        System.out.println("\nEjecutando constructor CalculatorService");
+    }
+
+    public String holaMundo(){
+      return "Hola Mundo!";
+    }
+}
---
+++ spring-helloworld/src/main/java/com/example/Main.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        CalculatorService calculadora = (CalculatorService) context.getBean("calculatorService");
+
+        String texto = calculadora.holaMundo();
+        System.out.println(texto);
+
+        CalculatorService calculadora2 = (CalculatorService) context.getBean("calculatorService");
+
+        texto = calculadora2.holaMundo();
+        System.out.println(texto);
+    }
+}
