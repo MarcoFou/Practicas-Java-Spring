fb64951df056eceda3371cde28b0a312c782dee9
Marco Fouad <abboud1995@gmail.com>
2023-01-03 15:04:25 -0300

    Spring HelloWorld Beans XML

---
+++ spring-helloworld/src/main/java/com/example/Calculadora.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Calculadora {
+
+
+    public Calculadora() {
+
+        System.out.println("Ejecutando constructor Calculadora");
+
+    }
+
+    public String holaMundo(){
+      return "Hola Mundo!";
+    }
+}
